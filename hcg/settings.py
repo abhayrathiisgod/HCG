afc8fb915e30f3c6bf0ee2b03525c5949a4d750d
Abhay Rathi <abhayrathi50@gmail.com>
2024-06-03 13:37:42 +0545

    mail created

--- hcg/settings.py
+++ hcg/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'abhayrathi448@gmail.com'  
-EMAIL_HOST_PASSWORD = 'cqtrwvhcsveujydi' 
+EMAIL_HOST_PASSWORD = 
